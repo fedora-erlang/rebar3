c42a687914e54690beca583b043a483273d95dbb
Peter Lemenkov <lemenkov@gmail.com>
2022-02-04 20:47:33 +0100

    Unbundle hex_core ver. 0.7.1
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- apps/rebar/src/rebar_httpc_adapter.erl
+++ apps/rebar/src/rebar_httpc_adapter.erl
@@ -1,7 +1,7 @@
 %% Derived from hex_core v0.7.1 for extra flexibility.
 
 -module(rebar_httpc_adapter).
--behaviour(r3_hex_http).
+-behaviour(hex_http).
 -export([request/5]).
 
 %%====================================================================
